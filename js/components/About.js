c1d5e80f2963cf20d5a0965aefd41ba344a44c86
robkot500 <robert.kotwicki5@gmail.com>
2020-06-09 07:50:08 +0200

    almost portfolio

--- js/components/About.js
+++ js/components/About.js
@@ -4,7 +4,20 @@ import { Element } from "react-scroll";
 function About() {
     return (
         <Element name='about' className='about-container'>
-            <h1>About</h1>
+            <div className='about-up'>
+                <p className='about-welcome'>Hi, I'm Robert. Nice to meet you</p>
+                <div className='about-wrapper'>
+                    <div className='about-picture'></div>
+                    <div className="about-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia odio exercitationem id voluptatem eos. Magni, aspernatur dignissimos totam sit reprehenderit alias voluptatem illum tempora maiores, libero deserunt autem repellendus rem.</div>
+                </div>
+            </div>
+            <div className='about-down'>
+                <div className='about-middle'>
+                    <div className='about-column'></div>
+                    <div className='about-column'></div>
+                    <div className='about-column'></div>
+                </div>
+            </div>
         </Element>
     )
 }
--- js/components/Porftolio.js
+++ js/components/Porftolio.js
@@ -4,8 +4,29 @@ import { Element } from "react-scroll";
 function Portfolio() {
     return (
         <Element name='portfolio' className='portfolio-container'>
-            <h1>Portfolio</h1>
-        </Element>
+
+            <div className="portfolio-item">
+                <div className="portfolio-left">
+                    <p className='portfolio-title'>Weather forecast</p>
+                    <p className='portfolio-technologies'>HTML, CSS, SASS, REACT, REST API, JSON</p>
+                    <p className='portfolio-text'>A web application that allows user to view the weather forecast for 6 days using the Fetch API. Available for for any location by using geolocation or city name.</p>
+                    <div class="btn-container">
+                        <div class='btn-1'>Visit site</div>
+                        <div class='btn-2'>View code</div>
+                    </div>
+
+
+                </div>
+                <div className="portfolio-right"><a href="https://robkot500.github.io/Weather/build/" target="_blank"><img src="/../assets/weather.png" alt="Prognoza pogody" /></a></div>
+            </div>
+
+            <div className="portfolio-item">
+                <div className="portfolio-left">
+                    <p className='portfolio-title'>Mediator sądowy</p>
+                </div>
+                <div className="portfolio-right"><a href="https://robkot500.github.io/Mediacje/build" target="_blank"><img src="/../assets/mediator.png" alt="Mediator sądowy Iwona Kotwicka" /></a></div>
+            </div>
+        </Element >
     )
 }
 
