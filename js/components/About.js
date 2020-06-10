b218148d017709f7c23f2512a89a65b366ad5069
robkot500 <robert.kotwicki5@gmail.com>
2020-06-10 22:08:04 +0200

    next

--- js/components/About.js
+++ js/components/About.js
@@ -14,18 +14,18 @@ function About() {
                 </div>
                 <div className='about-middle'>
                     <div className='about-column'>
-                        <div className='about-icon'><img src="/../assets/education.png" alt="education" /></div>
+                        <div className='about-icon'><img src="./../assets/education.png" alt="education" /></div>
                         <p className='about-title'>Education</p>
                         <p className='about-column-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia molestiae praesentium consectetur velit quia exercitationem atque fugiat perspiciatis. Eum amet iure animi corrupti et laborum officiis recusandae architecto distinctio? Doloremque?</p>
                     </div>
                     <div className='about-column'>
-                        <div className='about-icon'><img src="/../assets/technologies.png" alt="technologies" /></div>
+                        <div className='about-icon'><img src="./../assets/technologies.png" alt="technologies" /></div>
                         <p className='about-title'>Skills</p>
                         <p className='about-column-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia molestiae praesentium consectetur velit quia exercitationem atque fugiat perspiciatis. Eum amet iure animi corrupti et laborum officiis recusandae architecto distinctio? Doloremque?</p>
 
                     </div>
                     <div className='about-column'>
-                        <div className='about-icon'><img src="/../assets/experience.png" alt="experience" /></div>
+                        <div className='about-icon'><img src="./../assets/experience.png" alt="experience" /></div>
                         <p className='about-title'>Experience</p>
                         <p className='about-column-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia molestiae praesentium consectetur velit quia exercitationem atque fugiat perspiciatis. Eum amet iure animi corrupti et laborum officiis recusandae architecto distinctio? Doloremque?</p>
                     </div>
