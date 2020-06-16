09bb1bddd04986694ec974f039b91ac4a09cdf8f
robkot500 <robert.kotwicki5@gmail.com>
2020-06-16 21:11:38 +0200

    next

--- js/components/About.js
+++ js/components/About.js
@@ -25,7 +25,7 @@ function About() {
                     <div className='about-frame'>
                         <div className='about-picture'></div>
                     </div>
-                    <div className="about-text">I`m a hard working and dedicated junior front-end developer. I have completed Coders Lab course, JS Developer: React, but I`m still learning. Right now I want to focus on React and Redux. I enjoy making usuful websites using HTML, CSS,  JavaScript and React JS.
+                    <div className="about-text">I`m a hard working and dedicated junior front-end developer.I enjoy making usuful websites using HTML, CSS,  JavaScript and React JS.
                         If you have any questions, do not hesitate to contact me.</div>
                 </div>
                 <div className='about-middle'>
@@ -45,12 +45,11 @@ function About() {
                         <div className='about-skill-wrapper'><i class="fab fa-js-square"></i><div className='about-skill'>JavaScript</div></div>
                         <div className='about-skill-wrapper'><i class="fab fa-react"></i><div className='about-skill'>React JS</div></div>
                         <div className='about-skill-wrapper'><i class="fab fa-sass"></i><div className='about-skill'>Sass</div></div>
-
                         <div className='about-skill-wrapper2'><i class="fab fa-git-alt"></i><div className='about-skill'>Git</div></div>
                         <div className='about-skill-wrapper2'><i class="fab fa-github-square"></i><div className='about-skill'>Github</div></div>
                         <div className='about-skill-wrapper2'><i class="fab fa-npm"></i><div className='about-skill'>npm</div></div>
-                        <div className='about-skill-wrapper2'><i class="fab fa-react"></i><div className='about-skill'>React JS</div></div>
-                        <div className='about-skill-wrapper2'><i class="fab fa-react"></i><div className='about-skill'>React JS</div></div>
+                        <div className='about-skill-wrapper2'><img src="./../assets/redux.png" alt="redux" /><div className='about-skill'>Redux</div></div>
+                        <div className='about-skill-wrapper2'><img src="./../assets/psd.png" alt="redux" /><div className='about-skill'>Photoshop</div></div>
                         <div onClick={handleClick} class="arrow bounce"></div>
                         <p className='about-column-text'>Contact me if you have any questions about my skills level.</p>
 
@@ -58,7 +57,9 @@ function About() {
                     <div className='about-column'>
                         <div className='about-icon'><img src="./../assets/experience.png" alt="experience" /></div>
                         <p className='about-title'>Experience</p>
-                        <p className='about-column-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia molestiae praesentium consectetur velit quia exercitationem atque fugiat perspiciatis. Eum amet iure animi corrupti et laborum officiis recusandae architecto distinctio? Doloremque?</p>
+                        <p className='about-column-text'>Although I have not got any comercial experience I`m coding and practising almost every day. I have experience in working in the Scrum metodology- a group project simulating the daily work of a programmer. Scrum Lab was a simulation of one developer sprint and had a strictly planned schedule.
+                        It incuded among other things: communication and cooperation in a team, scrum and canban metodologies, trello and github tools, gitflow and code review.
+</p>
                     </div>
                 </div>
             </div>
