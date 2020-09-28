832d8f826f0b767c4161dfadf6c7a54fcd18e953
robkot500 <robert.kotwicki5@gmail.com>
2020-09-28 09:11:51 +0200

    next

--- js/components/Intro.js
+++ js/components/Intro.js
@@ -6,7 +6,7 @@ function Intro() {
             <div className=" intro-wrapper">
                 <h2>Robert Kotwicki,</h2>
                 <p>a junior front end developer</p>
-                <div className="btn">
+                <div className="btn" id='view my work'>
                     <Link smooth={true} to="portfolio">View my work</Link>
                 </div>
             </div>
